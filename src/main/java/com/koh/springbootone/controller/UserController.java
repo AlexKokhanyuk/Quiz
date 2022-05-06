7ebc21aa4c44dfa8d5ccc4d973c4a4bd0446972c
ayeayedp <aye.aye.dp@gmail.com>
2022-05-06 08:12:02 +0300

    add select quiz

--- src/main/java/com/koh/springbootone/controller/UserController.java
+++ src/main/java/com/koh/springbootone/controller/UserController.java
@@ -17,14 +17,15 @@ import java.util.List;
 @Controller
 public class UserController {
     private UserService userService;
-    private QuizServise quizServise;
-    private QuestionServise questionServise;
+    private QuizServise quizService;
+    private QuestionServise questionService;
 
     @Autowired
-    public UserController(UserService userService, QuizServise quizServise, QuestionServise questionServise) {
+    public UserController(UserService userService, QuizServise quizService, QuestionServise questionService) {
         this.userService = userService;
-        this.quizServise=quizServise;
-        this.questionServise=questionServise;
+        this.quizService = quizService;
+
+        this.questionService = questionService;
     }
 
     @GetMapping("/users")
@@ -52,25 +53,38 @@ public class UserController {
     }
 
     @GetMapping("/user-update/{id}")
-    public String updateUserForm(@PathVariable("id") Long id, Model model){
-        User user=userService.findById(id);
-        model.addAttribute("user",user);
+    public String updateUserForm(@PathVariable("id") Long id, Model model) {
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
         return "user-update";
     }
 
     @PostMapping("/user-update")
-    public String updateUser(User user){
+    public String updateUser(User user) {
         userService.saveUser(user);
         return "redirect:/users";
     }
 
     @GetMapping("/quiz")
     public String findAllQuiz(Model model) {
-        List<Quiz> quiz = quizServise.findAll();
+        List<Quiz> quiz = quizService.findAll();
         model.addAttribute("quiz", quiz);
 
-        questionServise.findAll(1L);
         return "quiz";
     }
 
+    @GetMapping("/select-quiz/{id}")
+    public String selectQuiz(@PathVariable("id") Long id, Model model) {
+        Quiz quiz = quizService.findById(id);
+        model.addAttribute("quiz", quiz);
+        return "select-quiz";
+    }
+    @GetMapping("/pass-quiz/{id}")
+    public String passQuiz(@PathVariable("id") Long id, Model model) {
+        Quiz quiz = quizService.findById(id);
+        model.addAttribute("quiz", quiz);
+
+        return "pass-quiz";
+    }
 }
+
